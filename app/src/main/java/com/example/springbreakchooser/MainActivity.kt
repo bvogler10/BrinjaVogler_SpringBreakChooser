0cc5f30219d48ca2bf82e8116a5b633e9b0c4926
bvogler10 <brinjavogler@gmail.com>
2024-03-14 19:38:21 -0400

    able to do speech to text for english, german, and chinese

--- app/src/main/java/com/example/springbreakchooser/MainActivity.kt
+++ app/src/main/java/com/example/springbreakchooser/MainActivity.kt
@@ -5,7 +5,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import android.widget.Toast
 import java.lang.Exception
@@ -13,23 +16,29 @@ import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_CODE_SPEECH_INPUT = 100
-    private lateinit var textDisplay: TextView
+    private lateinit var textDisplay: EditText
+    private val languages = arrayOf("English", "German", "Chinese")
+    private val languageCodes = arrayOf("en-US", "de-DE", "zh-CN")
+    private lateinit var languageListView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textDisplay = findViewById(R.id.textView)
-        val voiceButton: Button = findViewById(R.id.speechToText)
-        voiceButton.setOnClickListener {
-            speak()
+        textDisplay = findViewById(R.id.enteredText)
+        languageListView = findViewById(R.id.languageListView)
+        languageListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, languages)
+        languageListView.choiceMode = ListView.CHOICE_MODE_SINGLE
+        languageListView.setOnItemClickListener { _, _, position, _ ->
+            val selectedLanguage = languageCodes[position]
+            speak(selectedLanguage)
         }
     }
 
-    private fun speak() {
+    private fun speak(language: String) {
         val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language)
         mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi say something")
 
         try {
@@ -46,7 +55,7 @@ class MainActivity : AppCompatActivity() {
             REQUEST_CODE_SPEECH_INPUT -> {
                 if (resultCode == Activity.RESULT_OK && null != data) {
                     val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-                    textDisplay.text = result?.get(0) ?: ""
+                    textDisplay.setText(result?.get(0) ?: "")
                 }
             }
         }
